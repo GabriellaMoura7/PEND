81208f23bed22ce9bfe18a52fc09d8c820b7db70
Gabriella Del Vigna de Moura <159180575+GabriellaMoura7@users.noreply.github.com>
2025-02-20 12:52:55 -0300

    Add files via upload

---
+++ ExerciciosVariaveis.js
@@ -0,0 +1,78 @@
+1)
+
+let nome = "Gabriella";
+let idade = "16";
+let cidade = "Votorantim";
+
+console.log("Nome:", nome, "/ Idade:", idade, "/ Cidade", cidade);
+
+2)
+
+var 2nome; (inválido) / Forma correta: var nome2;
+
+var _idade; (válido) 
+
+var cidade-natal; (inválido) / Forma correta: var cidadeNatal;
+
+var $preco; (inválido) / var preco;
+
+var left; (inválido) / Forma correta: As duas são váraveis, então não tem como resolver.
+
+3)
+
+let altura;
+
+console.log(altura);
+
+Aparece que é indefinido, pois não atribui um número.
+
+4)
+
+let altura = "1.59"
+
+console.log("Altura:", altura);
+
+5)
+
+Deu certo, porém por não ter atribuido algo ele fica dando aviso.
+
+6)
+
+let contador = 0;
+
+contador = contador + 1;
+
+contador = contador + 1;
+
+contador = contador + 1;
+
+console.log("O valor é igual á:", contador, ".");
+
+7)
+
+Não tem como, pois é uma constante, portanto ela não muda.
+
+8)
+
+O var é global. Ex: var altura;
+
+O let é um bloco. Ex: let largura;
+
+O Constante é para armazenar valor. Ex: const diga = "Abacate";
+
+9)
+
+Está errado por que o b foi uma váriavel criada dentro do if.
+
+10) 
+
+Vai aparecer uma mensagem dizendo que uma variável não foi definida.
+
+11)
+
+Irá Aparecer o nome de Carlos primeiro porque ele trocou o nome de Ana para Carlos, e depois ele volta para o primeiro valor que é a Ana.
+
+12)
+
+Não mudou tanto, só vai aparecer que o bloco tanto por dentro como por fora são 30.
+
